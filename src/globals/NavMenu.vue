eb0a32dc1a83caa95e247aeb695712c6907e6ed8
Ali <alifais1122@gmail.com>
2022-12-25 17:36:04 +0300

    Failed to implement i18-vue for localization, because the vite package for it's syntax is in conflict with some vuetify packages (kill me)

--- src/globals/NavMenu.vue
+++ src/globals/NavMenu.vue
@@ -1,48 +1,26 @@
 <template>
-  <VBtn @click="changeTheme('lightTheme')">light</VBtn>
-  <VBtn @click="changeTheme('darkTheme')">dark</VBtn>
-  <VBtn @click="changeTheme('redTheme')">red</VBtn>
-  <VBtn @click="changeTheme('otherTheme')">other</VBtn>
   <p>{{ t('hello') }}</p>
-  <VBtn>toggle language</VBtn>
+  <button @click="changeLanguage">Change langauge</button>
 </template>
 
 <script>
-import { useI18n } from "vue-i18n";
+import { defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-export default {
+export default defineComponent({
+  name: 'HelloI18n',
   setup() {
     const { t } = useI18n({
       inheritLocale: true,
-      useScope: "local",
-    });
-
-    return { t };
-  },
-
-  methods: {
-    changeTheme(theme) {
-      this.$vuetify.theme.global.name = theme;
-    },
-    changeLocale(locale) {
-        this.$i18n.locale = locale;
-    },
-  },
-};
-</script>
-
-<style>
-</style>
-
-<i18n>
-    {
-      "en" : {
-        "hello": "Hello i18n in SFC!"
-      },
-      "ara" : {
-        "hello": "لماذا نحن هنا"
-      }
-    }
-
-</i18n>
+      useScope: 'local'
+    })
     
+    return { t }
+  },
+  methods: {
+    changeLanguage() {
+      this.$i18n.locale === "ara" ? this.$i18n.locale = "en" : this.$i18n.locale = "ara";
+    }
+  }
+})
+</script>
